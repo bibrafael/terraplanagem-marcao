b592300365d6870cff642a23a23b3b904374c170
bibrafael@gmail.com <bibrafael@gmail.com@4f52f2de-312e-8bb2-081e-3675a6edf785>
2014-01-22 02:28:56 +0000

    Modificações no código do Login.

--- trunk/Terraplanagem/src/view/FramePrincipal.java
+++ trunk/Terraplanagem/src/view/FramePrincipal.java
@@ -23,140 +23,141 @@ public class FramePrincipal extends JFrame {
 	private JMenuItem menuItemRelatorio;
 	private JMenuItem menuItemTransporte;
 	private JMenuItem menuItemSair;
-	
-	public FramePrincipal () {
-		this.setTitle("Terraplanagem São Marcos");
+
+	public FramePrincipal() {
+		this.setTitle( "Terraplanagem São Marcos" );
 		menuBar = new JMenuBar();
-		menu = new JMenu("Cadastros");
-		menuItemCliente = new JMenuItem("Cliente");
-		menuItemCliente.addActionListener(new ClienteListener());
-		menu.add(menuItemCliente);
-		menuItemProduto = new JMenuItem("Produto");
-		menuItemProduto.addActionListener(new ProdutoListener());
-		menu.add(menuItemProduto);
-		menuItemUsuario = new JMenuItem("Usuário");
-		menuItemUsuario.addActionListener(new UsuarioListener());
-		menu.add(menuItemUsuario);
-		menuBar.add(menu);
-		
-		menu = new JMenu("Serviços");
-		menuItemTransporte = new JMenuItem("Transportes");
-		menuItemTransporte.addActionListener(new TransporteListener());
-		menu.add(menuItemTransporte);
-		menuItemVendas = new JMenuItem("Vendas");
-		menuItemVendas.addActionListener(new VendasListener());
-		menu.add(menuItemVendas);
-		menuItemRelatorio = new JMenuItem("Relatório de Transporte");
-		menuItemRelatorio.addActionListener(new RelatorioTransporteListener());
-		menu.add(menuItemRelatorio);
-		menuItemRelatorio = new JMenuItem("Relatório de Vendas");
-		menuItemRelatorio.addActionListener(new RelatorioVendasListener());
-		menu.add(menuItemRelatorio);
-		menuBar.add(menu);
-		
-		menu = new JMenu("Sair");
-		menuItemSair = new JMenuItem("Fechar Programa");
-		menuItemSair.addActionListener(new FecharProgramaListener());
-		menu.add(menuItemSair);
-		menuBar.add(menu);
-		
-		this.setJMenuBar(menuBar);
-		this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		menu = new JMenu( "Cadastros" );
+		menuItemCliente = new JMenuItem( "Cliente" );
+		menuItemCliente.addActionListener( new ClienteListener() );
+		menu.add( menuItemCliente );
+		menuItemProduto = new JMenuItem( "Produto" );
+		menuItemProduto.addActionListener( new ProdutoListener() );
+		menu.add( menuItemProduto );
+		menuItemUsuario = new JMenuItem( "Usuário" );
+		menuItemUsuario.addActionListener( new UsuarioListener() );
+		menu.add( menuItemUsuario );
+		menuBar.add( menu );
+
+		menu = new JMenu( "Serviços" );
+		menuItemTransporte = new JMenuItem( "Transportes" );
+		menuItemTransporte.addActionListener( new TransporteListener() );
+		menu.add( menuItemTransporte );
+		menuItemVendas = new JMenuItem( "Vendas" );
+		menuItemVendas.addActionListener( new VendasListener() );
+		menu.add( menuItemVendas );
+		menuItemRelatorio = new JMenuItem( "Relatório de Transporte" );
+		menuItemRelatorio.addActionListener( new RelatorioTransporteListener() );
+		menu.add( menuItemRelatorio );
+		menuItemRelatorio = new JMenuItem( "Relatório de Vendas" );
+		menuItemRelatorio.addActionListener( new RelatorioVendasListener() );
+		menu.add( menuItemRelatorio );
+		menuBar.add( menu );
+
+		menu = new JMenu( "Sair" );
+		menuItemSair = new JMenuItem( "Fechar Programa" );
+		menuItemSair.addActionListener( new FecharProgramaListener() );
+		menu.add( menuItemSair );
+		menuBar.add( menu );
+
+		this.setJMenuBar( menuBar );
+		this.setDefaultCloseOperation( WindowConstants.EXIT_ON_CLOSE );
 	}
-	
+
 	private class RelatorioTransporteListener implements ActionListener {
 
-		@SuppressWarnings("deprecation")
+		@SuppressWarnings( "deprecation" )
 		@Override
-		public void actionPerformed(ActionEvent e) {
+		public void actionPerformed( ActionEvent e ) {
 			JDialog dialog = new RelatorioTransporteDialog();
 			dialog.pack();
-			dialog.show(true);
+			dialog.show( true );
 		}
-		
+
 	}
-	
+
 	private class FecharProgramaListener implements ActionListener {
 
 		@Override
-		public void actionPerformed(ActionEvent e) {
-			System.exit(EXIT_ON_CLOSE);
+		public void actionPerformed( ActionEvent e ) {
+			System.exit( EXIT_ON_CLOSE );
 		}
-		
+
 	}
-	
+
 	private class RelatorioVendasListener implements ActionListener {
 
-		@SuppressWarnings("deprecation")
+		@SuppressWarnings( "deprecation" )
 		@Override
-		public void actionPerformed(ActionEvent e) {
+		public void actionPerformed( ActionEvent e ) {
 			JDialog dialog = new RelatorioVendasDialog();
 			dialog.pack();
-			dialog.show(true);
+			dialog.show( true );
 		}
-		
+
 	}
-	
+
 	private class VendasListener implements ActionListener {
 
-		@SuppressWarnings("deprecation")
+		@SuppressWarnings( "deprecation" )
 		@Override
-		public void actionPerformed(ActionEvent e) {
-			JDialog dialog = new VendasDialog(true);
-			dialog.setModal(true);
-			dialog.setSize(800, 500);
-			dialog.show(true);
+		public void actionPerformed( ActionEvent e ) {
+			JDialog dialog = new VendasDialog( true );
+			dialog.setModal( true );
+			dialog.setSize( 800, 500 );
+			dialog.show( true );
 		}
 	}
-	
+
 	private class TransporteListener implements ActionListener {
 
-		@SuppressWarnings("deprecation")
+		@SuppressWarnings( "deprecation" )
 		@Override
-		public void actionPerformed(ActionEvent e) {
-			JDialog dialog = new VendasDialog(false);
-			dialog.setModal(true);
-			dialog.setSize(800, 500);
-			dialog.show(true);			
+		public void actionPerformed( ActionEvent e ) {
+			JDialog dialog = new VendasDialog( false );
+			dialog.setModal( true );
+			dialog.setSize( 800, 500 );
+			dialog.show( true );
 		}
-		
+
 	}
-	
+
 	private class ClienteListener implements ActionListener {
 
-		@SuppressWarnings("deprecation")
+		@SuppressWarnings( "deprecation" )
 		@Override
-		public void actionPerformed(ActionEvent e) {
+		public void actionPerformed( ActionEvent e ) {
 			JDialog dialog = new ClienteDialog();
-			dialog.setModal(true);
-			dialog.setSize(800, 500);
-			dialog.show(true);
+			dialog.setModal( true );
+			dialog.setSize( 800, 500 );
+			dialog.show( true );
 		}
 	}
-	
+
 	private class ProdutoListener implements ActionListener {
 
-		@SuppressWarnings("deprecation")
+		@SuppressWarnings( "deprecation" )
 		@Override
-		public void actionPerformed(ActionEvent e) {
+		public void actionPerformed( ActionEvent e ) {
 			JDialog dialog = new ProdutoDialog();
-			dialog.setModal(true);
-			dialog.setSize(800, 500);
-			dialog.show(true);
+			dialog.setModal( true );
+			dialog.setSize( 800, 500 );
+			dialog.show( true );
 		}
 	}
-	
+
 	private class UsuarioListener implements ActionListener {
 
-		@SuppressWarnings("deprecation")
+		@SuppressWarnings( "deprecation" )
 		@Override
-		public void actionPerformed(ActionEvent e) {
+		public void actionPerformed( ActionEvent e ) {
 			JDialog dialog = new UsuarioDialog();
-			dialog.setModal(true);
-			dialog.setSize(800, 500);
-			dialog.show(true);			
+			dialog.setModal( true );
+			dialog.setSize( 800, 500 );
+			dialog.setLocationRelativeTo( null );
+			dialog.show( true );
 		}
-		
+
 	}
-	
+
 }
--- trunk/Terraplanagem/src/view/Login.java
+++ trunk/Terraplanagem/src/view/Login.java
@@ -5,6 +5,8 @@ import java.awt.Font;
 import java.awt.HeadlessException;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
@@ -33,21 +35,25 @@ public class Login extends JFrame {
 	private JPanel contentPane;
 	private JPasswordField pfSenha;
 	private JTextField tfUsuario;
+	private JButton btnSair;
+	private JButton btnLogin;
+	private JLabel lblUsuario;
+	private JLabel lblSenha;
 
 	/**
 	 * Launch the application.
 	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
+	public static void main( String[] args ) {
+		EventQueue.invokeLater( new Runnable() {
 			public void run() {
 				try {
 					Login frame = new Login();
-					frame.setVisible(true);
-				} catch (Exception e) {
+					frame.setVisible( true );
+				} catch ( Exception e ) {
 					e.printStackTrace();
 				}
 			}
-		});
+		} );
 	}
 
 	/**
@@ -56,93 +62,152 @@ public class Login extends JFrame {
 	 * @throws UnsupportedLookAndFeelException
 	 */
 	public Login() throws UnsupportedLookAndFeelException {
-		super("Terraplanagem São Marcos");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 350, 180);
-		setLocationRelativeTo(null);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
-		setResizable(false);
+		super( "Terraplanagem São Marcos" );
+		setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+		setBounds( 100, 100, 350, 180 );
+		setLocationRelativeTo( null );
+		setContentPane( getContentPane() );
+		setResizable( false );
 
-		UIManager.setLookAndFeel(new MetalLookAndFeel());
+		UIManager.setLookAndFeel( new MetalLookAndFeel() );
 
-		JLabel lblUsuario = new JLabel("Usuário");
-		lblUsuario.setFont(new Font("Arial", Font.BOLD, 13));
-		lblUsuario.setHorizontalAlignment(SwingConstants.LEFT);
-		lblUsuario.setBounds(44, 25, 80, 20);
-		contentPane.add(lblUsuario);
-
-		tfUsuario = new JTextField();
-		tfUsuario.setFont(new Font("Arial", Font.BOLD, 13));
-		tfUsuario.setBounds(138, 25, 150, 20);
-		contentPane.add(tfUsuario);
-
-		JLabel lblSenha = new JLabel("Senha");
-		lblSenha.setFont(new Font("Arial", Font.BOLD, 13));
-		lblSenha.setBounds(44, 60, 80, 20);
-		contentPane.add(lblSenha);
-
-		pfSenha = new JPasswordField();
-		pfSenha.setFont(new Font("Arial", Font.BOLD, 13));
-		pfSenha.setBounds(138, 60, 150, 20);
-		contentPane.add(pfSenha);
-
-		JButton btnLogin = new JButton("Login");
-		btnLogin.setFont(new Font("Arial", Font.BOLD, 13));
-		btnLogin.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				String nome = tfUsuario.getText().toString();
-				String senha = pfSenha.getText().toString();
-				if (nome.isEmpty() && senha.isEmpty()) {
-					JOptionPane.showMessageDialog(null, "Digite o usuário e a senha!", "Atenção", JOptionPane.INFORMATION_MESSAGE);
-				} else if (nome.isEmpty()) {
-					JOptionPane.showMessageDialog(null, "Digite o usuário!", "Atenção", JOptionPane.INFORMATION_MESSAGE);
-				} else if (senha.isEmpty()) {
-					JOptionPane.showMessageDialog(null, "Digite a senha do usuário!", "Atenção", JOptionPane.INFORMATION_MESSAGE);
-				} else if ((!nome.isEmpty()) && (!senha.isEmpty())) {
-					try {
-						senha = criptografarSenha(senha);
-					} catch (HeadlessException e1) {
-						e1.printStackTrace();
-					} catch (NoSuchAlgorithmException e1) {
-						e1.printStackTrace();
-					}
-
-					boolean flag = AcoesUsuario.getInstance().verificarLogin(nome, senha);
-
-					if (flag == false) {
-						JOptionPane.showConfirmDialog(null, "Usuário ou senha incorretos!", "Alerta", JOptionPane.CLOSED_OPTION, JOptionPane.WARNING_MESSAGE);
-					} else {
-						JFrame frame = new FramePrincipal();
-						frame.setSize(600, 400);
-						frame.show();
-						dispose();
-					}
-				}
-			}
-		});
-		btnLogin.setBounds(52, 112, 89, 25);
-		contentPane.add(btnLogin);
-
-		JButton btnSair = new JButton("Sair");
-		btnSair.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				System.exit(JFrame.EXIT_ON_CLOSE);
-			}
-		});
-		btnSair.setFont(new Font("Arial", Font.BOLD, 13));
-		btnSair.setBounds(193, 112, 89, 25);
-		contentPane.add(btnSair);
+		initialize();
 	}
 
-	protected String criptografarSenha(String senha) throws NoSuchAlgorithmException {
-		MessageDigest md = MessageDigest.getInstance("MD5");
-		BigInteger hash = new BigInteger(1, md.digest(senha.getBytes()));
-		String s = hash.toString(16);
-		if (s.length() % 2 != 0)
+	private void initialize() {
+		contentPane.add( getLblUsuario() );
+		contentPane.add( getTfUsuario() );
+		contentPane.add( getLblSenha() );
+		contentPane.add( getPfSenha() );
+		contentPane.add( getBtnLogin() );
+		contentPane.add( getBtnSair() );
+	}
+
+	protected String criptografarSenha( String senha ) throws NoSuchAlgorithmException {
+		MessageDigest md = MessageDigest.getInstance( "MD5" );
+		BigInteger hash = new BigInteger( 1, md.digest( senha.getBytes() ) );
+		String s = hash.toString( 16 );
+		if ( s.length() % 2 != 0 )
 			s = "0" + s;
 		return s;
+	}
+
+	public JPasswordField getPfSenha() {
+		if ( pfSenha == null ) {
+			pfSenha = new JPasswordField();
+			pfSenha.addKeyListener( new KeyAdapter() {
+				@Override
+				public void keyPressed( KeyEvent e ) {
+					if ( e.getKeyCode() == KeyEvent.VK_ENTER ) {
+						getBtnLogin().requestFocusInWindow();
+						getBtnLogin().doClick();
+					}
+				}
+			} );
+			pfSenha.setFont( new Font( "Arial", Font.BOLD, 13 ) );
+			pfSenha.setBounds( 138, 60, 150, 20 );
+		}
+		return pfSenha;
+	}
+
+	public JButton getBtnSair() {
+		if ( btnSair == null ) {
+			btnSair = new JButton( "Sair" );
+			btnSair.addActionListener( new ActionListener() {
+				public void actionPerformed( ActionEvent e ) {
+					System.exit( JFrame.EXIT_ON_CLOSE );
+				}
+			} );
+			btnSair.setFont( new Font( "Arial", Font.BOLD, 13 ) );
+			btnSair.setBounds( 193, 112, 89, 25 );
+		}
+		return btnSair;
+	}
+
+	public JButton getBtnLogin() {
+		if ( btnLogin == null ) {
+			btnLogin = new JButton( "Login" );
+			btnLogin.setFont( new Font( "Arial", Font.BOLD, 13 ) );
+			btnLogin.addActionListener( new ActionListener() {
+				public void actionPerformed( ActionEvent e ) {
+					String nome = tfUsuario.getText().trim().toString();
+					String senha = pfSenha.getText().toString();
+					if ( nome.isEmpty() && senha.isEmpty() ) {
+						JOptionPane.showMessageDialog( null, "Digite o usuário e a senha!", "Atenção", JOptionPane.INFORMATION_MESSAGE );
+					} else if ( nome.isEmpty() ) {
+						JOptionPane.showMessageDialog( null, "Digite o usuário!", "Atenção", JOptionPane.INFORMATION_MESSAGE );
+					} else if ( senha.isEmpty() ) {
+						JOptionPane.showMessageDialog( null, "Digite a senha do usuário!", "Atenção", JOptionPane.INFORMATION_MESSAGE );
+					} else if ( (!nome.isEmpty()) && (!senha.isEmpty()) ) {
+						try {
+							senha = criptografarSenha( senha );
+						} catch ( HeadlessException e1 ) {
+							e1.printStackTrace();
+						} catch ( NoSuchAlgorithmException e1 ) {
+							e1.printStackTrace();
+						}
+
+						boolean flag = AcoesUsuario.getInstance().verificarLogin( nome, senha );
+
+						if ( flag == false ) {
+							JOptionPane.showConfirmDialog( null, "Usuário ou senha incorretos!", "Alerta", JOptionPane.CLOSED_OPTION,
+									JOptionPane.WARNING_MESSAGE );
+						} else {
+							JFrame frame = new FramePrincipal();
+							frame.setSize( 600, 400 );
+							frame.show();
+							frame.setLocationRelativeTo( null );
+							dispose();
+						}
+					}
+				}
+			} );
+			btnLogin.setBounds( 52, 112, 89, 25 );
+		}
+		return btnLogin;
+	}
+
+	public JTextField getTfUsuario() {
+		if ( tfUsuario == null ) {
+			tfUsuario = new JTextField();
+			tfUsuario.addKeyListener( new KeyAdapter() {
+				@Override
+				public void keyPressed( KeyEvent e ) {
+					if ( e.getKeyCode() == KeyEvent.VK_ENTER )
+						pfSenha.requestFocusInWindow();
+				}
+			} );
+			tfUsuario.setFont( new Font( "Arial", Font.BOLD, 13 ) );
+			tfUsuario.setBounds( 138, 25, 150, 20 );
+		}
+		return tfUsuario;
+	}
+
+	public JLabel getLblUsuario() {
+		if ( lblUsuario == null ) {
+			lblUsuario = new JLabel( "Usuário" );
+			lblUsuario.setFont( new Font( "Arial", Font.BOLD, 13 ) );
+			lblUsuario.setHorizontalAlignment( SwingConstants.LEFT );
+			lblUsuario.setBounds( 44, 25, 80, 20 );
+		}
+		return lblUsuario;
+	}
+
+	public JLabel getLblSenha() {
+		if ( lblSenha == null ) {
+			lblSenha = new JLabel( "Senha" );
+			lblSenha.setFont( new Font( "Arial", Font.BOLD, 13 ) );
+			lblSenha.setBounds( 44, 60, 80, 20 );
+		}
+		return lblSenha;
+	}
+
+	public JPanel getContentPane() {
+		if ( contentPane == null ) {
+			contentPane = new JPanel();
+			contentPane.setBorder( new EmptyBorder( 5, 5, 5, 5 ) );
+			contentPane.setLayout( null );
+		}
+		return contentPane;
 	}
 }
