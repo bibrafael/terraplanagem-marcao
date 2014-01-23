eb359cb5f26df1d7f8deab7673c0e7390726b515
bibrafael@gmail.com <bibrafael@gmail.com@4f52f2de-312e-8bb2-081e-3675a6edf785>
2014-01-23 02:20:57 +0000

    Teste com a lib SwingBits

--- trunk/Terraplanagem/src/view/ClienteDialog.java
+++ trunk/Terraplanagem/src/view/ClienteDialog.java
@@ -56,161 +56,160 @@ public class ClienteDialog extends JDialog {
 	private JRadioButton rbCnpj;
 	private ButtonGroup grupo;
 
-	private JPanel panelButtons; 	
+	private JPanel panelButtons;
 	private JButton btCriar;
 	private JButton btAlterar;
 	private JButton btDeletar;
 	private JButton btLimpar;
 
-	private JScrollPane scrollPanel;	
+	private JScrollPane scrollPanel;
 	private JTable table;
 	private DefaultTableModel tableModel;
 
 	public ClienteDialog() {
-		this.setTitle("Terraplanagem São Marcos - Cadastro de Clientes");
+		this.setTitle( "Terraplanagem São Marcos - Cadastro de Clientes" );
 		GridBagConstraints cons = new GridBagConstraints();
-		cons.insets = new Insets(5, 5, 5, 5);
+		cons.insets = new Insets( 5, 5, 5, 5 );
 
 		panelToolBar = new JPanel();
-		panelToolBar.setLayout(new BorderLayout());
+		panelToolBar.setLayout( new BorderLayout() );
 
 		panelItens = new JPanel();
-		panelItens.setLayout(new GridBagLayout());		
-		id = new Long(0);
+		panelItens.setLayout( new GridBagLayout() );
+		id = new Long( 0 );
 
-		lbNome = new JLabel("Nome:");
+		lbNome = new JLabel( "Nome:" );
 		cons.gridy = 0;
 		cons.gridx = 0;
 		cons.anchor = GridBagConstraints.LINE_START;
-		panelItens.add(lbNome, cons);
+		panelItens.add( lbNome, cons );
 
-		tfNome = new JTextField(20);
+		tfNome = new JTextField( 20 );
 		cons.gridy = 0;
 		cons.gridx = 1;
-		tfNome.addKeyListener(new pesquisaClienteListener());
-		panelItens.add(tfNome, cons);
+		tfNome.addKeyListener( new pesquisaClienteListener() );
+		panelItens.add( tfNome, cons );
 
-		lbTelefone = new JLabel("Telefone:");
+		lbTelefone = new JLabel( "Telefone:" );
 		cons.gridy = 0;
 		cons.gridx = 2;
-		panelItens.add(lbTelefone, cons);
+		panelItens.add( lbTelefone, cons );
 
-		tfTelefone = new JFormattedTextField(setMascara("(##)####-####"));
-		tfTelefone.setColumns(10);
+		tfTelefone = new JFormattedTextField( setMascara( "(##)####-####" ) );
+		tfTelefone.setColumns( 10 );
 		cons.gridy = 0;
 		cons.gridx = 3;
-		panelItens.add(tfTelefone, cons);
+		panelItens.add( tfTelefone, cons );
 
-		lbEndereco = new JLabel("Endereço:");
+		lbEndereco = new JLabel( "Endereço:" );
 		cons.gridy = 1;
 		cons.gridx = 0;
-		panelItens.add(lbEndereco, cons);
+		panelItens.add( lbEndereco, cons );
 
-		tfEndereco = new JTextField(20);
+		tfEndereco = new JTextField( 20 );
 		cons.gridy = 1;
 		cons.gridx = 1;
-		panelItens.add(tfEndereco, cons);
+		panelItens.add( tfEndereco, cons );
 
-		lbCidade = new JLabel("Cidade:");
+		lbCidade = new JLabel( "Cidade:" );
 		cons.gridy = 1;
 		cons.gridx = 2;
-		panelItens.add(lbCidade, cons);
+		panelItens.add( lbCidade, cons );
 
-		tfCidade = new JTextField(15);
+		tfCidade = new JTextField( 15 );
 		cons.gridy = 1;
 		cons.gridx = 3;
-		panelItens.add(tfCidade, cons);
+		panelItens.add( tfCidade, cons );
 
-		rbCpf = new JRadioButton("CPF");
-		rbCpf.setActionCommand("cpf");
-		rbCpf.setSelected(true);
-		rbCpf.addActionListener(new documentoClienteListener());
+		rbCpf = new JRadioButton( "CPF" );
+		rbCpf.setActionCommand( "cpf" );
+		rbCpf.setSelected( true );
+		rbCpf.addActionListener( new documentoClienteListener() );
 		cons.gridy = 2;
 		cons.gridx = 2;
-		panelItens.add(rbCpf, cons);
+		panelItens.add( rbCpf, cons );
 
-		rbCnpj = new JRadioButton("CNPJ");
-		rbCnpj.setActionCommand("cnpj");
-		rbCnpj.addActionListener(new documentoClienteListener());
+		rbCnpj = new JRadioButton( "CNPJ" );
+		rbCnpj.setActionCommand( "cnpj" );
+		rbCnpj.addActionListener( new documentoClienteListener() );
 		cons.gridy = 2;
 		cons.gridx = 3;
-		panelItens.add(rbCnpj, cons);
+		panelItens.add( rbCnpj, cons );
 
 		grupo = new ButtonGroup();
-		grupo.add(rbCpf);
-		grupo.add(rbCnpj);
+		grupo.add( rbCpf );
+		grupo.add( rbCnpj );
 
-		lbDocumento = new JLabel("Documento:");
+		lbDocumento = new JLabel( "Documento:" );
 		cons.gridy = 2;
 		cons.gridx = 0;
-		panelItens.add(lbDocumento, cons);
+		panelItens.add( lbDocumento, cons );
 
-		tfDocumento = new JFormattedTextField(setMascara("###.###.###-##"));
-		tfDocumento.setColumns(20);
+		tfDocumento = new JFormattedTextField( setMascara( "###.###.###-##" ) );
+		tfDocumento.setColumns( 20 );
 		cons.gridy = 2;
 		cons.gridx = 1;
-		panelItens.add(tfDocumento, cons);
-		
-		btLimpar = new JButton("Limpar");
+		panelItens.add( tfDocumento, cons );
+
+		btLimpar = new JButton( "Limpar" );
 		cons.gridy = 3;
 		cons.gridx = 1;
-		btLimpar.addActionListener(new limparCliente());
-		panelItens.add(btLimpar, cons);
+		btLimpar.addActionListener( new limparCliente() );
+		panelItens.add( btLimpar, cons );
 
-		panelToolBar.add(panelItens, BorderLayout.PAGE_START);
+		panelToolBar.add( panelItens, BorderLayout.PAGE_START );
 
 		panelButtons = new JPanel();
-		panelButtons.setLayout(new FlowLayout());
-		
-		btCriar = new JButton("Inserir");
-		btCriar.addActionListener(new criarClienteListener());
-		panelButtons.add(btCriar);
-		
+		panelButtons.setLayout( new FlowLayout() );
 
-		btAlterar = new JButton("Alterar");
-		btAlterar.addActionListener(new alterarClienteListener());
-		panelButtons.add(btAlterar);				
+		btCriar = new JButton( "Inserir" );
+		btCriar.addActionListener( new criarClienteListener() );
+		panelButtons.add( btCriar );
 
-		btDeletar = new JButton("Deletar");
-		btDeletar.addActionListener(new deletarClienteListener());
-		panelButtons.add(btDeletar);		
+		btAlterar = new JButton( "Alterar" );
+		btAlterar.addActionListener( new alterarClienteListener() );
+		panelButtons.add( btAlterar );
 
-		//Montando a tabela
-		tableModel = AcoesCliente.getInstance().ler("");
-		table = new JTable(tableModel){
+		btDeletar = new JButton( "Deletar" );
+		btDeletar.addActionListener( new deletarClienteListener() );
+		panelButtons.add( btDeletar );
+
+		// Montando a tabela
+		tableModel = AcoesCliente.getInstance().ler( "" );
+		table = new JTable( tableModel ) {
 			private static final long serialVersionUID = 1L;
 
-			public boolean isCellEditable(int row, int col) {
+			public boolean isCellEditable( int row, int col ) {
 				return false;
 			}
 		};
-		table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-		table.addMouseListener(new selecionarLinhaTabela());
-		table.getColumnModel().removeColumn( table.getColumnModel().getColumn(0));
+		table.setSelectionMode( ListSelectionModel.SINGLE_SELECTION );
+		table.addMouseListener( new selecionarLinhaTabela() );
+		table.getColumnModel().removeColumn( table.getColumnModel().getColumn( 0 ) );
 		setarTamanhoColunas();
 
-		scrollPanel = new JScrollPane(table);		
+		scrollPanel = new JScrollPane( table );
 
-		//Mostrando o Frame
-		this.setLayout(new BorderLayout());
-		this.add(panelToolBar, BorderLayout.PAGE_START);
-		this.add(scrollPanel, BorderLayout.CENTER);
-		this.add(panelButtons, BorderLayout.PAGE_END);
-		this.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+		// Mostrando o Frame
+		this.setLayout( new BorderLayout() );
+		this.add( panelToolBar, BorderLayout.PAGE_START );
+		this.add( scrollPanel, BorderLayout.CENTER );
+		this.add( panelButtons, BorderLayout.PAGE_END );
+		this.setDefaultCloseOperation( WindowConstants.DISPOSE_ON_CLOSE );
 	}
-	
+
 	private class limparCliente implements ActionListener {
 
 		@Override
-		public void actionPerformed(ActionEvent e) {
-			tfNome.setText(null);
-			tfEndereco.setText(null);
-			tfCidade.setText(null);
-			tfDocumento.setText(null);
-			tfTelefone.setText(null);
+		public void actionPerformed( ActionEvent e ) {
+			tfNome.setText( null );
+			tfEndereco.setText( null );
+			tfCidade.setText( null );
+			tfDocumento.setText( null );
+			tfTelefone.setText( null );
 			id = null;
-			table.setModel(AcoesCliente.getInstance().ler(tfNome.getText()));
-			table.getColumnModel().removeColumn( table.getColumnModel().getColumn(0));
+			table.setModel( AcoesCliente.getInstance().ler( tfNome.getText() ) );
+			table.getColumnModel().removeColumn( table.getColumnModel().getColumn( 0 ) );
 			setarTamanhoColunas();
 		}
 	}
@@ -218,59 +217,65 @@ public class ClienteDialog extends JDialog {
 	private class pesquisaClienteListener implements KeyListener {
 
 		@Override
-		public void keyPressed(KeyEvent e) {}
+		public void keyPressed( KeyEvent e ) {
+		}
+
 		@Override
-		public void keyReleased(KeyEvent e) {
-			table.setModel(AcoesCliente.getInstance().ler(tfNome.getText()));
-			table.getColumnModel().removeColumn( table.getColumnModel().getColumn(0));
+		public void keyReleased( KeyEvent e ) {
+			table.setModel( AcoesCliente.getInstance().ler( tfNome.getText() ) );
+			table.getColumnModel().removeColumn( table.getColumnModel().getColumn( 0 ) );
 			setarTamanhoColunas();
-			tfEndereco.setText(null);
-			tfCidade.setText(null);
-			tfDocumento.setText(null);
-			tfTelefone.setText(null);
+			tfEndereco.setText( null );
+			tfCidade.setText( null );
+			tfDocumento.setText( null );
+			tfTelefone.setText( null );
 			id = null;
 		}
+
 		@Override
-		public void keyTyped(KeyEvent e) {}
+		public void keyTyped( KeyEvent e ) {
+		}
 
 	}
 
-	class CenterRenderer extends DefaultTableCellRenderer {  
+	class CenterRenderer extends DefaultTableCellRenderer {
 		private static final long serialVersionUID = 4433840270638649209L;
 
-		public CenterRenderer() {  
-			setHorizontalAlignment(CENTER);  
-		}  
-	}  
-	public void setarTamanhoColunas() {
-		table.getColumnModel().getColumn(0).setPreferredWidth(175);
-		table.getColumnModel().getColumn(1).setPreferredWidth(100);
-		table.getColumnModel().getColumn(2).setPreferredWidth(225);
-		table.getColumnModel().getColumn(3).setPreferredWidth(50);
-		table.getColumnModel().getColumn(4).setPreferredWidth(50);
+		public CenterRenderer() {
+			setHorizontalAlignment( CENTER );
+		}
+	}
 
-		TableCellRenderer centerRenderer = new CenterRenderer();  
-		TableColumn column = table.getColumnModel().getColumn(3);  
-		column.setCellRenderer(centerRenderer); 
+	public void setarTamanhoColunas() {
+		table.getColumnModel().getColumn( 0 ).setPreferredWidth( 175 );
+		table.getColumnModel().getColumn( 1 ).setPreferredWidth( 100 );
+		table.getColumnModel().getColumn( 2 ).setPreferredWidth( 225 );
+		table.getColumnModel().getColumn( 3 ).setPreferredWidth( 50 );
+		table.getColumnModel().getColumn( 4 ).setPreferredWidth( 50 );
+
+		TableCellRenderer centerRenderer = new CenterRenderer();
+		TableColumn column = table.getColumnModel().getColumn( 3 );
+		column.setCellRenderer( centerRenderer );
 	}
 
 	private class criarClienteListener implements ActionListener {
 
 		@Override
-		public void actionPerformed(ActionEvent e) {
-			if(tfNome.getText().isEmpty()) {
-				JOptionPane.showMessageDialog(null, "Digite o nome do cliente!", "Alerta", JOptionPane.WARNING_MESSAGE);
-			}else {
-				table.setModel(AcoesCliente.getInstance().criar(tfNome.getText(), tfEndereco.getText(), tfCidade.getText(), tfDocumento.getText(), tfTelefone.getText()));
-				table.getColumnModel().removeColumn( table.getColumnModel().getColumn(0));
+		public void actionPerformed( ActionEvent e ) {
+			if ( tfNome.getText().isEmpty() ) {
+				JOptionPane.showMessageDialog( null, "Digite o nome do cliente!", "Alerta", JOptionPane.WARNING_MESSAGE );
+			} else {
+				table.setModel( AcoesCliente.getInstance().criar( tfNome.getText(), tfEndereco.getText(), tfCidade.getText(), tfDocumento.getText(),
+						tfTelefone.getText() ) );
+				table.getColumnModel().removeColumn( table.getColumnModel().getColumn( 0 ) );
 				setarTamanhoColunas();
-				tfNome.setText(null);
-				tfEndereco.setText(null);
-				tfCidade.setText(null);
-				tfDocumento.setText(null);
-				tfTelefone.setText(null);
+				tfNome.setText( null );
+				tfEndereco.setText( null );
+				tfCidade.setText( null );
+				tfDocumento.setText( null );
+				tfTelefone.setText( null );
 				id = null;
-				JOptionPane.showMessageDialog(null, "Cliente cadastrado com sucesso.", "Sucesso", JOptionPane.INFORMATION_MESSAGE);
+				JOptionPane.showMessageDialog( null, "Cliente cadastrado com sucesso.", "Sucesso", JOptionPane.INFORMATION_MESSAGE );
 			}
 		}
 
@@ -279,18 +284,19 @@ public class ClienteDialog extends JDialog {
 	private class alterarClienteListener implements ActionListener {
 
 		@Override
-		public void actionPerformed(ActionEvent e) {
-			if(tfNome.getText().isEmpty()) {
-				JOptionPane.showMessageDialog(null, "Digite o nome do cliente!", "Alerta", JOptionPane.WARNING_MESSAGE);
-			}else {
-				table.setModel(AcoesCliente.getInstance().atualizar(id, tfNome.getText(), tfEndereco.getText(), tfCidade.getText(), tfDocumento.getText(), tfTelefone.getText()));
-				table.getColumnModel().removeColumn( table.getColumnModel().getColumn(0));
+		public void actionPerformed( ActionEvent e ) {
+			if ( tfNome.getText().isEmpty() ) {
+				JOptionPane.showMessageDialog( null, "Digite o nome do cliente!", "Alerta", JOptionPane.WARNING_MESSAGE );
+			} else {
+				table.setModel( AcoesCliente.getInstance().atualizar( id, tfNome.getText(), tfEndereco.getText(), tfCidade.getText(), tfDocumento.getText(),
+						tfTelefone.getText() ) );
+				table.getColumnModel().removeColumn( table.getColumnModel().getColumn( 0 ) );
 				setarTamanhoColunas();
-				tfNome.setText(null);
-				tfEndereco.setText(null);
-				tfCidade.setText(null);
-				tfDocumento.setText(null);
-				tfTelefone.setText(null);
+				tfNome.setText( null );
+				tfEndereco.setText( null );
+				tfCidade.setText( null );
+				tfDocumento.setText( null );
+				tfTelefone.setText( null );
 				id = null;
 			}
 		}
@@ -300,54 +306,48 @@ public class ClienteDialog extends JDialog {
 	private class deletarClienteListener implements ActionListener {
 
 		@Override
-		public void actionPerformed(ActionEvent e) {
-			if(id == null) {
-				JOptionPane.showMessageDialog(null, "Selecione um usuário para deletar!", "Erro", JOptionPane.ERROR_MESSAGE);
-			}else {
-				Object[] options = { "Sim", "Não" };   
-				int sd =   
-					JOptionPane.showOptionDialog(   
-							null,   
-							"Deseja deletar cliente?",   
-							"Alerta",   
-							JOptionPane.DEFAULT_OPTION,   
-							JOptionPane.WARNING_MESSAGE,   
-							null,   
-							options,   
-							options[0]);  
-				if (sd == 0) {   
-					table.setModel(AcoesCliente.getInstance().deletar(id));
-					table.getColumnModel().removeColumn( table.getColumnModel().getColumn(0));
+		public void actionPerformed( ActionEvent e ) {
+			if ( id == null ) {
+				JOptionPane.showMessageDialog( null, "Selecione um usuário para deletar!", "Erro", JOptionPane.ERROR_MESSAGE );
+			} else {
+				Object[] options = { "Sim", "Não" };
+				int sd = JOptionPane.showOptionDialog( null, "Deseja deletar cliente?", "Alerta", JOptionPane.DEFAULT_OPTION, JOptionPane.WARNING_MESSAGE,
+						null, options, options[0] );
+				if ( sd == 0 ) {
+					table.setModel( AcoesCliente.getInstance().deletar( id ) );
+					table.getColumnModel().removeColumn( table.getColumnModel().getColumn( 0 ) );
 					setarTamanhoColunas();
-					tfNome.setText(null);
-					tfEndereco.setText(null);
-					tfCidade.setText(null);
-					tfDocumento.setText(null);
-					tfTelefone.setText(null);
+					tfNome.setText( null );
+					tfEndereco.setText( null );
+					tfCidade.setText( null );
+					tfDocumento.setText( null );
+					tfTelefone.setText( null );
 					id = null;
-				}else {}
+				} else {
+				}
 			}
 		}
 	}
 
-	private MaskFormatter setMascara(String mascara){  
-		MaskFormatter mask = null;  
-		try{  
-			mask = new MaskFormatter(mascara);                    
-		}catch(java.text.ParseException ex){}  
-		return mask;  
-	}  
+	private MaskFormatter setMascara( String mascara ) {
+		MaskFormatter mask = null;
+		try {
+			mask = new MaskFormatter( mascara );
+		} catch ( java.text.ParseException ex ) {
+		}
+		return mask;
+	}
 
 	private class documentoClienteListener implements ActionListener {
 
 		@Override
-		public void actionPerformed(ActionEvent e) {
-			if(grupo.getSelection().getActionCommand().equals("cnpj")) {
-				DefaultFormatterFactory factory = new DefaultFormatterFactory(setMascara("##.###.###/####-##"));
-				tfDocumento.setFormatterFactory(factory);
-			}else {
-				DefaultFormatterFactory factory = new DefaultFormatterFactory(setMascara("###.###.###-##"));
-				tfDocumento.setFormatterFactory(factory);
+		public void actionPerformed( ActionEvent e ) {
+			if ( grupo.getSelection().getActionCommand().equals( "cnpj" ) ) {
+				DefaultFormatterFactory factory = new DefaultFormatterFactory( setMascara( "##.###.###/####-##" ) );
+				tfDocumento.setFormatterFactory( factory );
+			} else {
+				DefaultFormatterFactory factory = new DefaultFormatterFactory( setMascara( "###.###.###-##" ) );
+				tfDocumento.setFormatterFactory( factory );
 			}
 		}
 	}
@@ -355,36 +355,40 @@ public class ClienteDialog extends JDialog {
 	private class selecionarLinhaTabela implements MouseListener {
 
 		@Override
-		public void mouseClicked(MouseEvent e) {
+		public void mouseClicked( MouseEvent e ) {
 			int linha = table.getSelectedRow();
-			id = new Long(((String)table.getModel().getValueAt(linha, 0)));	
-			tfNome.setText((String)table.getModel().getValueAt(linha, 1));
-			if(table.getModel().getValueAt(linha, 2).toString().length() == 14) {
-				rbCpf.setSelected(true);
-				DefaultFormatterFactory factory = new DefaultFormatterFactory(setMascara("###.###.###-##"));
-				tfDocumento.setFormatterFactory(factory);
-			}else if(table.getModel().getValueAt(linha, 2).toString().length() == 18) {
-				rbCnpj.setSelected(true);
-				DefaultFormatterFactory factory = new DefaultFormatterFactory(setMascara("##.###.###/####-##"));
-				tfDocumento.setFormatterFactory(factory);
+			id = new Long( ((String) table.getModel().getValueAt( linha, 0 )) );
+			tfNome.setText( (String) table.getModel().getValueAt( linha, 1 ) );
+			if ( table.getModel().getValueAt( linha, 2 ).toString().length() == 14 ) {
+				rbCpf.setSelected( true );
+				DefaultFormatterFactory factory = new DefaultFormatterFactory( setMascara( "###.###.###-##" ) );
+				tfDocumento.setFormatterFactory( factory );
+			} else if ( table.getModel().getValueAt( linha, 2 ).toString().length() == 18 ) {
+				rbCnpj.setSelected( true );
+				DefaultFormatterFactory factory = new DefaultFormatterFactory( setMascara( "##.###.###/####-##" ) );
+				tfDocumento.setFormatterFactory( factory );
 			}
-			tfDocumento.setText((String)table.getModel().getValueAt(linha, 2));
-			tfEndereco.setText((String)table.getModel().getValueAt(linha, 3));
-			tfCidade.setText((String)table.getModel().getValueAt(linha, 4));
-			tfTelefone.setText((String)table.getModel().getValueAt(linha, 5));
+			tfDocumento.setText( (String) table.getModel().getValueAt( linha, 2 ) );
+			tfEndereco.setText( (String) table.getModel().getValueAt( linha, 3 ) );
+			tfCidade.setText( (String) table.getModel().getValueAt( linha, 4 ) );
+			tfTelefone.setText( (String) table.getModel().getValueAt( linha, 5 ) );
 
 		}
+
 		@Override
-		public void mouseEntered(MouseEvent e) {
+		public void mouseEntered( MouseEvent e ) {
 		}
+
 		@Override
-		public void mouseExited(MouseEvent e) {
+		public void mouseExited( MouseEvent e ) {
 		}
+
 		@Override
-		public void mousePressed(MouseEvent e) {
+		public void mousePressed( MouseEvent e ) {
 		}
+
 		@Override
-		public void mouseReleased(MouseEvent e) {
+		public void mouseReleased( MouseEvent e ) {
 		}
 	}
 }
